e4c3e5d17098ec0678662d740d93985563c4a901
MomenAbdelwadoud <momenwadoudg@gmail.com>
2024-03-14 22:36:44 +0300

    Add Create patient

--- app/components/doctor/Doctor.tsx
+++ app/components/doctor/Doctor.tsx
@@ -6,6 +6,8 @@ import DatePickerWithRange from "./DateRangePicker";
 import DataTable from "./DataTable";
 import {ColumnDef} from "@tanstack/react-table";
 import {columns} from "./columns";
+import NewPatient from "./NewPatient";
+import {AlertCircle, Cross, Plus, UserPlus, Users} from "lucide-react";
 
 const Doctor = async () => {
 	const pbAuth = cookies().get("pb_auth")?.value;
@@ -25,8 +27,9 @@ const Doctor = async () => {
 					Here is a summary of your patients
 				</h4>
 			</div>
-			<div className="pt-12">
+			<div className="pt-12 flex justify-between">
 				<DatePickerWithRange></DatePickerWithRange>
+				<NewPatient></NewPatient>
 			</div>
 			<div className="pt-4 flex gap-5 xl:gap-16 flex-wrap">
 				<Card className="flex flex-col border-[1px]shadow-sm max-w-[300px] rounded-xl min-w-[250px]">
@@ -34,36 +37,7 @@ const Doctor = async () => {
 						<CardTitle className="text-md font-medium">
 							Total New Patients
 						</CardTitle>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							width="18"
-							height="18"
-							viewBox="0 0 24 24"
-							fill="none"
-							stroke="currentColor"
-							strokeWidth="2"
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							className="lucide lucide-user-plus">
-							<path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-							<circle
-								cx="9"
-								cy="7"
-								r="4"
-							/>
-							<line
-								x1="19"
-								x2="19"
-								y1="8"
-								y2="14"
-							/>
-							<line
-								x1="22"
-								x2="16"
-								y1="11"
-								y2="11"
-							/>
-						</svg>
+						<UserPlus className="w-5 h-5"></UserPlus>
 					</CardHeader>
 					<CardContent className="flex flex-col items-start">
 						<div className="w-full flex justify-center">
@@ -76,26 +50,7 @@ const Doctor = async () => {
 						<CardTitle className="text-md font-medium">
 							Total Patients
 						</CardTitle>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							width="18"
-							height="18"
-							viewBox="0 0 24 24"
-							fill="none"
-							stroke="currentColor"
-							strokeWidth="2"
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							className="lucide lucide-users">
-							<path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-							<circle
-								cx="9"
-								cy="7"
-								r="4"
-							/>
-							<path d="M22 21v-2a4 4 0 0 0-3-3.87" />
-							<path d="M16 3.13a4 4 0 0 1 0 7.75" />
-						</svg>
+						<Users className="w-5 h-5"></Users>
 					</CardHeader>
 					<CardContent className="flex flex-col items-start">
 						<div className="w-full flex justify-center">
@@ -108,35 +63,7 @@ const Doctor = async () => {
 						<CardTitle className="text-md font-medium">
 							Critical Conditions
 						</CardTitle>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							width="18"
-							height="18"
-							viewBox="0 0 24 24"
-							fill="none"
-							stroke="currentColor"
-							strokeWidth="2"
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							className="lucide lucide-alert-circle">
-							<circle
-								cx="12"
-								cy="12"
-								r="10"
-							/>
-							<line
-								x1="12"
-								x2="12"
-								y1="8"
-								y2="12"
-							/>
-							<line
-								x1="12"
-								x2="12.01"
-								y1="16"
-								y2="16"
-							/>
-						</svg>
+						<AlertCircle className="w-5 h-5"></AlertCircle>
 					</CardHeader>
 					<CardContent className="flex flex-col items-start">
 						<div className="w-full flex justify-center">
@@ -149,19 +76,7 @@ const Doctor = async () => {
 						<CardTitle className="text-md font-medium">
 							Non-Critical Conditions
 						</CardTitle>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							width="18"
-							height="18"
-							viewBox="0 0 24 24"
-							fill="none"
-							stroke="currentColor"
-							strokeWidth="2"
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							className="lucide lucide-cross">
-							<path d="M11 2a2 2 0 0 0-2 2v5H4a2 2 0 0 0-2 2v2c0 1.1.9 2 2 2h5v5c0 1.1.9 2 2 2h2a2 2 0 0 0 2-2v-5h5a2 2 0 0 0 2-2v-2a2 2 0 0 0-2-2h-5V4a2 2 0 0 0-2-2h-2z" />
-						</svg>
+						<Cross className="w-5 h-5"></Cross>
 					</CardHeader>
 					<CardContent className="flex flex-col items-start">
 						<div className="w-full flex justify-center">
---
+++ app/components/doctor/NewPatient.tsx
@@ -0,0 +1,200 @@
+"use client";
+import React from "react";
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "@/components/ui/dialog";
+import {Label} from "@/components/ui/label";
+import {Cross, Plus} from "lucide-react";
+import {z} from "zod";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {useForm} from "react-hook-form";
+import {Button} from "@/components/ui/button";
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form";
+import {Input} from "@/components/ui/input";
+import {
+	Select,
+	SelectTrigger,
+	SelectValue,
+	SelectContent,
+	SelectItem,
+} from "@/components/ui/select";
+import {Textarea} from "@/components/ui/textarea";
+import {pbClient} from "@/lib/db";
+import Cookies from "js-cookie";
+
+const formSchema = z.object({
+	name: z.string().min(2).max(50),
+	email: z.string().email().min(2).max(50),
+	condition: z.enum(["green", "red"]),
+	notes: z.string().min(1),
+	prescription: z.string().optional(),
+	doctor: z.any().optional(),
+});
+const NewPatient = () => {
+	return (
+		<Dialog>
+			<DialogTrigger asChild>
+				<Button>
+					<Plus className="mr-2 h-4 w-4" />
+					New Patient
+				</Button>
+			</DialogTrigger>
+			<DialogContent>
+				<DialogHeader>
+					<DialogTitle>Add New Patient</DialogTitle>
+				</DialogHeader>
+				<NewPatientForm></NewPatientForm>
+			</DialogContent>
+		</Dialog>
+	);
+};
+
+const NewPatientForm = () => {
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			name: "",
+		},
+	});
+	async function onSubmit(values: z.infer<typeof formSchema>) {
+		// Add patient to pbClient
+		try {
+			const pbAuth = Cookies.get("pb_auth");
+			pbClient.authStore.loadFromCookie(pbAuth!);
+			const currentDoctorId = pbClient.authStore.model?.id;
+			values = {...values, doctor: currentDoctorId};
+			console.log(values);
+			await pbClient.collection("patients").create(values);
+			window.location.reload();
+		} catch (error) {
+			console.log(error);
+		}
+	}
+	return (
+		<Form {...form}>
+			<form
+				id="new-patient"
+				onSubmit={form.handleSubmit(onSubmit)}
+				className="space-y-8">
+				<FormField
+					control={form.control}
+					name="name"
+					render={({field}) => (
+						<FormItem>
+							<FormLabel>Name</FormLabel>
+							<FormControl>
+								<Input
+									placeholder="John Doe"
+									{...field}
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name="email"
+					render={({field}) => (
+						<FormItem>
+							<FormLabel>Email</FormLabel>
+							<FormControl>
+								<Input
+									placeholder="Email address"
+									type="email"
+									{...field}
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}></FormField>
+				<FormField
+					control={form.control}
+					name="condition"
+					render={({field}) => (
+						<FormItem>
+							<FormLabel>Condition</FormLabel>
+							<Select
+								onValueChange={field.onChange}
+								defaultValue={field.value}>
+								<FormControl>
+									<SelectTrigger>
+										<SelectValue placeholder="Select a condition" />
+									</SelectTrigger>
+								</FormControl>
+								<SelectContent>
+									<SelectItem
+										value="green"
+										className="text-green">
+										Green
+									</SelectItem>
+									<SelectItem
+										value="red"
+										className="text-red">
+										Red
+									</SelectItem>
+								</SelectContent>
+							</Select>
+							<FormMessage />
+						</FormItem>
+					)}></FormField>
+
+				<FormField
+					control={form.control}
+					name="notes"
+					render={({field}) => (
+						<FormItem>
+							<FormLabel>Notes</FormLabel>
+							<FormControl>
+								<Textarea
+									placeholder="Notes about the patient"
+									className="resize-none h-32"
+									{...field}
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name="prescription"
+					render={({field}) => (
+						<FormItem>
+							<FormLabel>Prescription</FormLabel>
+							<FormControl>
+								<Textarea
+									placeholder="Write a prescription for the patient"
+									className="resize-none h-32"
+									{...field}
+								/>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<Button
+					type="submit"
+					form="new-patient">
+					Add
+				</Button>
+			</form>
+		</Form>
+	);
+};
+
+export default NewPatient;
